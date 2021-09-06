235de1f730849ee2ec6761dc635a8b4135e47ae7
Omar <owalwe2@uic.edu>
2021-09-06 15:34:36 -0500

    Initial Commit

---
+++ proj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by I ໓໐ຖนt I on 9/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
